5fb472da0a3388d26b81f1858fb21963347ef276
Charles <ckubicek@localhost.(none)>
2010-10-24 19:51:00 +0100

    shake for random rhyme working in simulator

---
+++ Classes/MotionWindow.h
@@ -0,0 +1,16 @@
+//
+//  MotionWindow.h
+//  rhymeTimeNavigation
+//
+//  Created by Charles on 24/10/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+
+
+@interface MotionWindow : UIWindow {
+
+}
+
+@end
